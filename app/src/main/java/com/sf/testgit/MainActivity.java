b9ab8f9beb49458dc987931ca3d4ce35e87732ec
徐雨萧 <18889158767@163.com>
2017-03-21 15:32:21 +0800

    MainActivity进行了控件的声明。布局中加了一个控件。

--- app/src/main/java/com/sf/testgit/MainActivity.java
+++ app/src/main/java/com/sf/testgit/MainActivity.java
@@ -1,13 +1,16 @@
 package com.sf.testgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //控件的声明
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
     }
 }
