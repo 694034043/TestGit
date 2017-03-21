374cce1b50da0eccfa59adac219f7c30ad55a511
徐雨萧 <18889158767@163.com>
2017-03-21 15:35:10 +0800

    tv的点击监听

--- app/src/main/java/com/sf/testgit/MainActivity.java
+++ app/src/main/java/com/sf/testgit/MainActivity.java
@@ -2,6 +2,7 @@ package com.sf.testgit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
+        //设置tv的点击监听
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
